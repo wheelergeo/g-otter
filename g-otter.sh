1061c31718440d6f6227f45cb52e6e99cb6978df
arinux <riechan.ye@outlook.com>
2024-01-15 14:32:17 +0800

    Add help function and use cwgo instead of kitex

--- g-otter.sh
+++ g-otter.sh
@@ -4,49 +4,80 @@ CURDIR=$(cd $(dirname $0); pwd)
 GATEPATH="${CURDIR}/gateway"
 MICROPATH="${CURDIR}/micro"
 GENPATH="${CURDIR}/gen"
-IDLPATH="${GENPATH}/idl"
-TMPPATH="${CURDIR}/wheelergeo/g-otter-gen"
+TPLPATH="${CURDIR}/tpl"
+MICROTPLPATH="${TPLPATH}/kitex/server"
+GATETPLPATH="${TPLPATH}/hertz/server"
+IDLPATH="${CURDIR}/idl"
+TMPFPATH="${CURDIR}/wheelergeo"
+TMPPATH="${TMPFPATH}/g-otter-gen"
 GATEMODULE="github.com/wheelergeo/g-otter-gateway"
 MICROMODULE="github.com/wheelergeo/g-otter-micro"
 GENMODULE="github.com/wheelergeo/g-otter-gen"
 
+if [ ! -d $GENPATH ]; then
+    echo "Error: gen dir is not existed!"
+    exit 1
+fi
 
-while getopts ":gm:a:" opt
+while getopts ":gm:a:ud:h" opt
 do
     case $opt in
-        g)
+        h) #help
+            echo "-g create gateway"
+            echo "-m create micro server"
+            echo "-a update micro server"
+            echo "-u update gateway"
+            echo "-d delete micro server"
+            ;;
+        g) # create gateway
             if [ ! -d $GATEPATH ]; then
-              mkdir $GATEPATH
+                mkdir $GATEPATH
             fi
             cd $GATEPATH
-            hz new -idl ${IDLPATH}/gateway.thrift -module ${GATEMODULE}
+            hz new -idl ${IDLPATH}/gateway.thrift \
+                   -module ${GATEMODULE} \
+                   --customize_layout           ${GATETPLPATH}/layout.yaml  \
+                   --customize_layout_data_path ${GATETPLPATH}/data.json    \
+                   --customize_package          ${GATETPLPATH}/package.yaml
             go mod tidy
             ;;
-        m)
+        m) # create micro
             if [ ! -d $MICROPATH ]; then
-              mkdir $MICROPATH
+                mkdir $MICROPATH
             fi
             MICRO=$OPTARG
             MICROMODULE=${MICROMODULE}-${MICRO}
 
-            mkdir ${MICROPATH}/${MICRO}
+            if [ ! -d $MICROPATH/${MICRO} ]; then
+                mkdir ${MICROPATH}/${MICRO}
+            fi
             cd ${MICROPATH}/${MICRO}
-            go mod init ${MICROMODULE}
+
+            if [ ! -f $MICROPATH/${MICRO}/go.mod ]; then
+                go mod init ${MICROMODULE}
+            fi
 
             mkdir -p ${TMPPATH}
-            kitex -module ${MICROMODULE} -gen-path ../../wheelergeo/g-otter-gen ${IDLPATH}/${MICRO}.thrift
-            mv ../../wheelergeo/g-otter-gen/${MICRO} ../../gen/${MICRO}
-            rm -rf ../../wheelergeo
+            kitex -module ${MICROMODULE} \
+                  -gen-path ../../wheelergeo/g-otter-gen \
+                  ${IDLPATH}/${MICRO}.thrift
 
-            kitex -module ${MICROMODULE} -service ${MICRO} -use ${GENMODULE} ${IDLPATH}/${MICRO}.thrift
+            if [ -d ${GENPATH}/${MICRO} ]; then
+                rm -rf ${GENPATH}/${MICRO}
+            fi
+            mv ${TMPPATH}/${MICRO} ${GENPATH}/${MICRO}
+            rm -rf ${TMPFPATH}
+
+            cwgo server -service ${MICRO} \
+                  -module ${MICROMODULE} \
+                  -type RPC \
+                  -pass "-use ${GENMODULE}" \
+                  -template ${MICROTPLPATH} \
+                  -idl ${IDLPATH}/${MICRO}.thrift
             go mod edit -replace=${GENMODULE}=../../gen
             go mod tidy
             ;;
-        a)
-            if [ ! -d $GENPATH ]; then
-                echo "Error: gen dir is not existed!"
-                exit 1
-            fi
+        a) # update micro gen
             MICRO=$OPTARG
             MICROMODULE=${MICROMODULE}-${MICRO}
 
@@ -58,13 +89,42 @@ do
             cd ${MICROPATH}/${MICRO}
 
             mkdir -p ${TMPPATH}
-            kitex -module ${MICROMODULE} -gen-path ../../wheelergeo/g-otter-gen ${IDLPATH}/${MICRO}.thrift
+            kitex -module ${MICROMODULE} \
+                  -gen-path ../../wheelergeo/g-otter-gen \
+                  ${IDLPATH}/${MICRO}.thrift
 
             if [ -d ${GENPATH}/${MICRO} ]; then
                 rm -rf ${GENPATH}/${MICRO}
             fi
-            mv ../../wheelergeo/g-otter-gen/${MICRO} ../../gen/${MICRO}
-            rm -rf ../../wheelergeo
+            mv ${TMPPATH}/${MICRO} ${GENPATH}/${MICRO}
+            rm -rf ${TMPFPATH}
+
+            kitex -module ${MICROMODULE} \
+                  -use ${GENMODULE} \
+                  -template-dir ${MICROTPLPATH} \
+                  ${IDLPATH}/${MICRO}.thrift
+            ;;
+        u) # update gateway
+            if [ ! -d $GATEPATH ]; then
+                echo "Error: gateway dir is not existed!"
+                exit 1
+            fi
+            cd $GATEPATH
+            hz update -idl ${IDLPATH}/gateway.thrift \
+                      -module             ${GATEMODULE} \
+                      --customize_package ${GATETPLPATH}/package.yaml
+            go mod tidy
+            ;;
+        d) # delete micro
+            MICRO=$OPTARG
+
+            if [ -d ${MICROPATH}/${MICRO} ]; then
+                rm -rf ${MICROPATH}/${MICRO}
+            fi
+
+            if [ -d ${GENPATH}/${MICRO} ]; then
+                rm -rf ${GENPATH}/${MICRO}
+            fi
             ;;
         ?)
             echo "there is unrecognized parameter."
